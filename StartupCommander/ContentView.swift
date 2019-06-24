a9a200ec373010c8db22e08976b98891ee8166c3
Paul Wood <paul.wood.iii@gmail.com>
2019-06-24 17:28:59 -0400

    Initial Commit

---
+++ StartupCommander/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StartupCommander
+//
+//  Created by Paul Wood on 6/24/19.
+//  Copyright © 2019 Paul Wood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
